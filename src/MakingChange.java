12c009fe3a0b094fbe60cac4c08a6cf6386864d0
ericadavis <ericadavis@172.16.21.6>
2017-09-15 12:27:56 -0400

    Finds # of quarters, dimes, nickels, and pennies.

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -8,10 +8,13 @@ public class MakingChange {
 		//JOptionPane.showMessageDialog(null, "You entered " + num);
 
 		int cents = (int) (num * 100);
-		int nickles = cents/5;
-		int pennies = cents - nickles * 5;
+		int quarters = cents/25;
+		int dimes = (cents - quarters * 25)/10;
+		int nickles = (cents  - quarters *25 - dimes *10)/5;
+		int pennies = cents - quarters *25 - dimes *10 - nickles *5;
 		
-		JOptionPane.showMessageDialog(null, "$" + num + " is " + nickles  
+		JOptionPane.showMessageDialog(null, "$" + num + " is " + quarters + " quarters and " + dimes 
+				+ " dimes and "+ nickles  
 				+ " nickles and " + pennies + " pennies.");
 	}
 
