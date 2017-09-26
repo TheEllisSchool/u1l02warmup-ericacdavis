ff64d557539f313e1da7c94b4cb2096173faf709
ericadavis <ericadavis@172.16.21.6>
2017-09-26 08:23:02 -0400

    This includes average calculation along with penny, nickle, dime quarter, division

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -3,17 +3,20 @@ import javax.swing.JOptionPane;
 public class MakingChange {
 
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog(null, "Enter an amount using dollars and cents");
-		double num = Double.parseDouble(input);
+		String input = JOptionPane.showInputDialog(null, "Enter your first amount using dollars and cents");
+		double num1 = Double.parseDouble(input);
 		//JOptionPane.showMessageDialog(null, "You entered " + num);
-
-		int cents = (int) (num * 100);
+		input = JOptionPane.showInputDialog(null, "Enter a second amount of money in dollars and cents");
+		double num2 = Double.parseDouble(input);
+		
+		double average = (num1 + num2) /2;
+		int cents = (int) (average * 100);
 		int quarters = cents/25;
 		int dimes = (cents - quarters * 25)/10;
 		int nickles = (cents  - quarters *25 - dimes *10)/5;
 		int pennies = cents - quarters *25 - dimes *10 - nickles *5;
 		
-		JOptionPane.showMessageDialog(null, "$" + num + " is " + quarters + " quarters and " + dimes 
+		JOptionPane.showMessageDialog(null, "$" + average + " is " + quarters + " quarters and " + dimes 
 				+ " dimes and "+ nickles  
 				+ " nickles and " + pennies + " pennies.");
 		//this is a test
